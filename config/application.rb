b9c7b62113be21cfaeef765cd125bf95f228cd29
Alex <alexfulsome@gmail.com>
2018-06-13 16:57:34 -0600

    move response caching to /10

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module PriceApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.cache_store = :redis_store, ENV["REDIS_URL"]
+    config.cache_store = :redis_store, "#{ENV["REDIS_URL"]}/10"
     config.api_only = true
   end
 end
